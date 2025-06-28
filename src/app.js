cd692e72ac8f22b376c1e3bb4d5b58a10fa9efd9
Ravath <158808898+vathv123@users.noreply.github.com>
2025-06-28 14:52:23 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ const videoSources = [
 // ✅ Set video elements first
 document.querySelectorAll('.slideShowMain').forEach((item, index) => {
   item.innerHTML = `
-    <video class="w-full h-full object-cover trailer" muted loop playsinline></video>
+    <video class="w-full h-full object-cover trailer" muted loop playsinline> <source src="video/Myboo.mp4" type="video/mp4"> </video>
   `;
 });
 
